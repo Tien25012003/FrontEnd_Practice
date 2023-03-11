82e0c5e48e2886b360c6185e2045037d69bd2493
Tien25012003 <21520112@gm.uit.edu.vn>
2023-03-12 02:10:57 +0700

    skia_tesla

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import { View, Text, StatusBar } from "react-native";
-import React from "react";
-import Profile from "./PracticeUI/TabNavigationUI/screens/Profile"
-import Home from "./PracticeUI/TabNavigationUI/screens/Home"
-import { Provider } from 'react-redux';
-import NavigationRoot from "./PracticeUI/TabNavigationUI/NavigationRoot";
-import Rocket_Index from "./PracticeUI/RocketUI/Rocket_Index";
-import Login_Index from "./PracticeUI/LoginUI/Login_Index";
-import Food_Index from "./PracticeUI/FoodDeliveryUI/Food_Index";
-import Calculator_Index from "./PracticeUI/CalculatorUI/Calculator_Index";
-import ColorCard_Index from "./PracticeUI/ColorCard/ColorCard_Index";
+import {View, Text, StatusBar} from 'react-native';
+import React from 'react';
+import Profile from './PracticeUI/TabNavigationUI/screens/Profile';
+import Home from './PracticeUI/TabNavigationUI/screens/Home';
+import {Provider} from 'react-redux';
+import NavigationRoot from './PracticeUI/TabNavigationUI/NavigationRoot';
+import Rocket_Index from './PracticeUI/RocketUI/Rocket_Index';
+import Login_Index from './PracticeUI/LoginUI/Login_Index';
+import Food_Index from './PracticeUI/FoodDeliveryUI/Food_Index';
+import Calculator_Index from './PracticeUI/CalculatorUI/Calculator_Index';
+import ColorCard_Index from './PracticeUI/ColorCard/ColorCard_Index';
+import Charging from './PracticeUI/RNSkia/Tesla_Skia/Charging';
+import NavigationBottom from './PracticeUI/RNSkia/Tesla_Skia/NavigationBottom';
 const App = () => {
   return (
     <>
-      <StatusBar barStyle={'light-content'} backgroundColor={'transparent'} translucent />
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
       {/* <NavigationRoot /> */}
       {/* <Rocket_Index /> */}
       {/* <Login_Index /> */}
       {/* <Food_Index /> */}
       {/* <Calculator_Index /> */}
-      <ColorCard_Index />
+      {/* <ColorCard_Index /> */}
+      {/* <Charging /> */}
+      <NavigationBottom />
     </>
   );
 };
